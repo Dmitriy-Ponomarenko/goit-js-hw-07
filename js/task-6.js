982a342c9ac14f591587de1146d5d15a3ba74384
Yumfii <144070576+Yumfii@users.noreply.github.com>
2024-04-06 13:06:37 +0200

    task 5-6

--- js/task-6.js
+++ js/task-6.js
@@ -5,3 +5,43 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+createButton.addEventListener('click', createBoxes);
+destroyButton.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = input.value;
+  
+  if (amount < 1 || amount > 100) {
+    return;
+  }
+  
+  clearBoxes();
+  
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+    size += 10;
+  }
+  
+  input.value = '';
+}
+
+function destroyBoxes() {
+  clearBoxes();
+}
+
+function clearBoxes() {
+  while (boxesContainer.firstChild) {
+    boxesContainer.removeChild(boxesContainer.firstChild);
+  }
+}
